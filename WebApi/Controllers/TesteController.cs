c97ad59aebfa9290351d7983d48660642e825ff3
Daniel Amaro <daniel.amaro@btgpactual.com>
2021-05-16 16:25:23 -0300

    commit inicial

---
+++ WebApi/Controllers/TesteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TesteController : ControllerBase
+    {
+
+        [HttpGet("TesteGet")]
+        public IActionResult TesteGet()
+        {
+            return Ok(new { resposta = "Solicitação GET enviada com sucesso!" });
+        }
+
+        [HttpPost("TestePost")]
+        public IActionResult TestePost()
+        {
+            return Ok(new { resposta = "Solicitação POST enviada com sucesso!" });
+        }
+
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,72 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Api Teste",
+                        Version = "v1"
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UsePathBase(new PathString("/api"));
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.DocumentTitle = "Api Teste";
+                c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "Api Teste");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
